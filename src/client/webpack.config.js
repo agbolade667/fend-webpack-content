d353fa6246763404643b50b8e0e5d1cfeb93b3ad
Agbolade Omowole <agboladeomowole@gmail.com>
2020-05-06 05:39:40 +0100

    I specified a customized webpack entry and tested it. It worked perfectly and created a dist folder

---
+++ src/client/webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: "production",
+    entry: 'C:/Users/IUO Web Dev/Desktop/webpack/fend-webpack-content/src/client/index.js',
+}
